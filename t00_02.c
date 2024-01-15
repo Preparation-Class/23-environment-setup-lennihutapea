6710acfb35e46650be26bb98123bd6b7130e6d3b
lennihutapea <143679130+lennihutapea@users.noreply.github.com>
2024-01-15 11:23:14 +0700

    pempros

--- t00_02.c
+++ t00_02.c
@@ -1,9 +1,10 @@
-// NIM - NAMA
+// 12S23023 - Lenni Febriyani
 
 #include <stdio.h>
 
-int main(char **_argc, int _argv)
+int main(int _argv, char **_argc)
 {
-    // Show your code here ...
+    printf("Aloha!");
+
     return 0;
 }
